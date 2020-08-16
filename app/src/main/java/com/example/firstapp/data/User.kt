b43172b9aad70fb2e5ddc8360ef3d2ae4ab732db
Nichanun <nichanun.pl@rmuti.ac.th>
2020-08-16 19:20:22 +0700

    Update Database

--- app/src/main/java/com/example/firstapp/data/User.kt
+++ app/src/main/java/com/example/firstapp/data/User.kt
@@ -1,4 +1,12 @@
-package com.example.firstapp.data
+package com.example.firstapp.Database
 
-class User {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user_table")
+data class User(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val nickname: String,
+    val phoneNumber: Int
+)
